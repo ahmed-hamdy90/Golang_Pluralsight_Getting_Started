5a4be90113c200c7b09edd4cfa67bafe74e98d78
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-05-15 16:20:29 +0200

    Complete Chapter 02 (the only demo video 05) for Pluralsight Go: Getting Started course

---
+++ Ch02/05/main.go
@@ -0,0 +1,15 @@
+package main
+
+// This is an Import block, It allow multiple packages to be imported without
+// repeated the "import" keyword
+import (
+	"fmt"
+)
+
+/*
+The main function, when part of the main package, identifies the entry point of
+an application
+ */
+func main() {
+	fmt.Println("Hello, playground")
+}
