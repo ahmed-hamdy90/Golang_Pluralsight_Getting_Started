2142c4fcc2a3b45b5693009fd67d36f5cbe82bd0
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-05-29 02:53:33 +0200

    modify comments under User Model file into [Demo: Adding Variables to the Webservice] video 24 under Chapter 05 into Pluralsight Go: Getting Stated course

--- Ch05/24/models/user.go
+++ Ch05/24/models/user.go
@@ -1,14 +1,14 @@
 package models
 
-// create User model using Struct type
+// Create User model using Struct type
 type User struct {
 	ID        int
 	FirstName string
 	LastName  string
 }
 
-// declare global variables using var block
+// Declare global variables using var block
 var (
-	users  []*User // slice to save users instance collections
-	nextID = 1     // referred as insert unique ID (look line Database)
+	users  []*User // slice to save pointers of created users instance
+	nextID = 1     // referred as insert unique ID (look like Database)
 )
