bd949b3161bce35259770a723224fa2acfadb1b9
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-05-29 03:50:17 +0200

    Fix Bug of Wrong extention method for UserController struct type (so UserConstoller wasn't implement http.Handler interface) under both videos(30 and 31) under Chapter 06

--- Ch06/30/controllers/users.go
+++ Ch06/30/controllers/users.go
@@ -7,7 +7,7 @@ type UserController struct {}
 
 // Now bind method to created controller, after func keyword will set type which wanted to bind this method
 // on C# there extensions method principle, Example => void Test(this UserController, port int)
-func (uc UserController) ServeHttp(w http.ResponseWriter, r *http.Request) {
+func (uc UserController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// write string message to response with bytes slice
 	w.Write([]byte("Hello from the User Controller!"))
 }
