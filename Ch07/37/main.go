559b1ecd6c31f587955767b81b44fe0e2b0e3b62
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-05-30 18:47:07 +0200

    Complete [Creating Loops that Terminate Based on a Condition] Vedio(37) under Chapter 07 into Pluralsight Go: Getting Started Cource

---
+++ Ch07/37/main.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+	var i int
+
+	// Use loop till condition construct
+	for i < 5 {
+		// use println built-in method
+		println(i)
+		i++
+	}
+
+	// Use loop till condition construct with break under specific condition using IF condition
+	//for i < 5 {
+	//	println(i)
+	//	i++
+	//	if i == 3 {
+	//		break
+	//	}
+	//}
+
+	// Use loop till condition construct with skipping loop iteration under specific condition using IF condition
+	//for i < 5 {
+	//	println(i)
+	//	i++
+	//	if i == 3 {
+	//		continue
+	//	}
+	//	println("continuing...")
+	//}
+}
