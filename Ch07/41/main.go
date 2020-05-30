ce94d38087e2000d735e9de0a340ba69725b7a26
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-05-30 18:49:52 +0200

    Complete [Working with the Panic Function] Vedio(41) under Chapter 07 into Pluralsight Go: Getting Started Cource

---
+++ Ch07/41/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// Use built-in println method
+	println("Starting web server")
+
+	// do important thing
+
+	// Use panic built-in method
+	panic("Something bad just happened")
+
+	println("Web server Started")
+}
